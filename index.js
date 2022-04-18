198b5580cc7815e7b47ec7afcaff2105e7d6aa39
alejandra trujillo <at386855@gmail.com>
2022-04-17 21:16:00 -0500

    backend

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+require("dotenv").config();
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api/", require("./rutas/vehiculos"));
+
+app.set("port", process.env.PORT);
+app.listen(app.get("port"), () => {
+  console.log(`el servidor esta corriendo en el puerto ${app.get("port")}`);
+});
